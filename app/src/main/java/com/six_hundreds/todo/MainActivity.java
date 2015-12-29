596d0af03fde628b16d9a5cf1d1c5e6841f3127c
Sergey Opivalov <6hundreds@gmail.com>
2015-12-29 23:26:41 +0300

    Some minor changes

--- app/src/main/java/com/six_hundreds/todo/MainActivity.java
+++ app/src/main/java/com/six_hundreds/todo/MainActivity.java
@@ -1,15 +1,25 @@
 package com.six_hundreds.todo;
 
+import android.app.DialogFragment;
 import android.app.FragmentManager;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+//import android.widget.Toolbar;
 
+
+import com.six_hundreds.todo.adapter.TabAdapter;
+import com.six_hundreds.todo.dialog.AddingDialogTaskFragment;
 import com.six_hundreds.todo.fragment.SplashFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AddingDialogTaskFragment.AddingTaskListener{
 
     FragmentManager fragmentManager;
     PreferencesHelper preferencesHelper;
@@ -23,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         preferencesHelper = PreferencesHelper.getInstance();
         fragmentManager = getFragmentManager();
         runSplash();
+
+        setUI();
     }
 
     @Override
@@ -59,11 +71,59 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setUI() {
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         if (toolbar != null) {
-            toolbar.setTitleTextColor(getResources().getColor(R.color.text_white, null));
-            setActionBar(toolbar);
+            toolbar.setTitleTextColor(getResources().getColor(R.color.text_white));
+            setSupportActionBar(toolbar);
         }
 
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.current_task_tab));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.done_task_tab));
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        TabAdapter tabAdapter = new TabAdapter(fragmentManager,2);
+
+        viewPager.setAdapter(tabAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment addingTaskDialogFragment = new AddingDialogTaskFragment();
+                addingTaskDialogFragment.show(fragmentManager, "AddingTaskDialogFragment");
+            }
+        });
+
+    }
+
+
+    @Override
+    public void onTaskAdded() {
+        Toast.makeText(MainActivity.this, "Task added", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onTaskAddingCancel() {
+        Toast.makeText(MainActivity.this, "Task adding cancelled", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/six_hundreds/todo/adapter/TabAdapter.java
+++ app/src/main/java/com/six_hundreds/todo/adapter/TabAdapter.java
@@ -1,9 +1,41 @@
 package com.six_hundreds.todo.adapter;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.PagerAdapter;
+
+import com.six_hundreds.todo.fragment.CurrentTaskFragment;
+import com.six_hundreds.todo.fragment.DoneTaskFragment;
 
 /**
  * Created by six_hundreds on 26.12.15.
  */
-public class TabAdapter extends PagerAdapter {
+public class TabAdapter extends FragmentStatePagerAdapter {
+
+    private int numberOfTabs;
+
+    public TabAdapter(FragmentManager fm, int numberOfTabs) {
+        super(fm);
+        this.numberOfTabs = numberOfTabs;
+    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position){
+            case 0:
+                return new CurrentTaskFragment();
+            case 1 :
+                return new DoneTaskFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return numberOfTabs;
+    }
 }
